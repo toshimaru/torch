83adb8e123d019c00e85c80bc93cb3fed81851a5
toshimaru <me@toshimaru.net>
2024-01-03 13:29:02 +0900

    Create directory with `create_dir_all`  and `touch` file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+use std::io::Result;
+
 fn main() {
-    println!("Hello, world!");
+    match fs::create_dir_all("my_directory") {
+        Ok(_) => println!("Directory created successfully"),
+        Err(e) => println!("Error creating directory: {}", e),
+    }
+    match touch("example.txt") {
+        Ok(_) => println!("File created successfully"),
+        Err(e) => println!("Error creating file: {}", e),
+    }
+}
+
+fn touch(path: &str) -> Result<()> {
+    fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(path)?;
+    Ok(())
 }
