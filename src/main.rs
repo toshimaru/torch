4a4dd2de919c7a7fa0b9f0e4672384688f8ea797
Toshimaru <me@toshimaru.net>
2024-01-04 09:53:34 +0900

    Create a directory with `create_dir_all` / Create a file like `touch` command (#1)
    * Create directory with `create_dir_all`  and `touch` file
    
    * chore: Update the timestamp for a existing file
    
    * ci: Create test.yml
    
    Run `cargo build` + `cargo run`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::fs::{OpenOptions, create_dir_all};
+use std::io::Result;
+use filetime::{FileTime, set_file_times};
+
 fn main() {
-    println!("Hello, world!");
+    match create_dir_all("my_directory") {
+        Ok(_) => println!("Directory created successfully"),
+        Err(e) => println!("Error creating directory: {}", e),
+    }
+    match touch("example.txt") {
+        Ok(_) => println!("File created successfully"),
+        Err(e) => println!("Error creating file: {}", e),
+    }
+}
+
+fn touch(path: &str) -> Result<()> {
+    OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(path)?;
+    let now: FileTime = FileTime::now();
+    set_file_times(path, now, now)?;
+    Ok(())
 }
