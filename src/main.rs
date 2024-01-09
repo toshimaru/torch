7e38fab38cfbc586f210df7992d14c3b88474cef
toshimaru <me@toshimaru.net>
2024-01-09 12:40:04 +0900

    Receive arguments and create a file
    For example:
    
    ```
    ❯ cargo run a b c
       Compiling torch v0.1.0 (/Users/toshimaru/src/github.com/toshimaru/torch)
        Finished dev [unoptimized + debuginfo] target(s) in 0.52s
         Running `target/debug/torch a b c`
    File created successfully
    File created successfully
    File created successfully
    
    ❯ ls a b c
    a b c
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 use std::fs::{OpenOptions, create_dir_all};
 use std::io::Result;
 use filetime::{FileTime, set_file_times};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    files: Vec<String>,
+}
 
 fn main() {
+    let args = Args::parse();
+    for file in args.files {
+        match touch(file.as_str()) {
+            Ok(_) => println!("File created successfully"),
+            Err(e) => println!("Error creating file: {}", e),
+        }
+    }
+
     match create_dir_all("my_directory") {
         Ok(_) => println!("Directory created successfully"),
         Err(e) => println!("Error creating directory: {}", e),
-    }
-    match touch("example.txt") {
-        Ok(_) => println!("File created successfully"),
-        Err(e) => println!("Error creating file: {}", e),
     }
 }
 
