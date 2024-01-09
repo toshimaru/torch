f6ce7acde0fa89db00f9ea625f1f6ec29740f668
Toshimaru <me@toshimaru.net>
2024-01-09 17:52:17 +0900

    deps: Add clap / feat: Base CLI App implementation with tests (#2)
    * deps: Add clap to create Rust CLI App
    
    `$ cargo add clap --features derive`
    
    * Receive arguments and create a file
    
    For example:
    
    ```
    ❯ cargo run a b c
       Compiling torch v0.1.0 (/Users/toshimaru/src/github.com/toshimaru/torch)
        Finished dev [unoptimized + debuginfo] target(s) in 0.52s
         Running `target/debug/torch a b c`
    File created successfully
    File created successfully
    File created successfully
    
    ❯ ls a b c
    a b c
    ```
    
    * test: Add test for `touch` function
    
    * chore: Run `cargo fmt`

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,63 @@
-use std::fs::{OpenOptions, create_dir_all};
+use clap::Parser;
+use filetime::{set_file_times, FileTime};
+use std::fs::{create_dir_all, OpenOptions};
 use std::io::Result;
-use filetime::{FileTime, set_file_times};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    files: Vec<String>,
+}
 
 fn main() {
+    let args = Args::parse();
+    for file in args.files {
+        match touch(file.as_str()) {
+            Ok(_) => println!("File created successfully"),
+            Err(e) => println!("Error creating file: {}", e),
+        }
+    }
+
     match create_dir_all("my_directory") {
         Ok(_) => println!("Directory created successfully"),
         Err(e) => println!("Error creating directory: {}", e),
     }
-    match touch("example.txt") {
-        Ok(_) => println!("File created successfully"),
-        Err(e) => println!("Error creating file: {}", e),
-    }
 }
 
 fn touch(path: &str) -> Result<()> {
-    OpenOptions::new()
-        .create(true)
-        .write(true)
-        .open(path)?;
+    OpenOptions::new().create(true).write(true).open(path)?;
     let now: FileTime = FileTime::now();
     set_file_times(path, now, now)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+    use std::fs::metadata;
+    use std::path::Path;
+    use std::thread;
+    use std::time::Duration;
+
+    #[test]
+    fn test_touch_creates_file() {
+        let test_path = format!("{}.txt", std::module_path!());
+        assert!(!Path::new(&test_path).exists());
+        touch(&test_path).unwrap();
+        assert!(Path::new(&test_path).exists());
+        fs::remove_file(test_path).unwrap();
+    }
+
+    #[test]
+    fn test_touch_updates_timestamp() {
+        let test_path = format!("{}.txt", std::module_path!());
+        fs::File::create(&test_path).unwrap();
+        thread::sleep(Duration::from_secs(1));
+        touch(&test_path).unwrap();
+        let metadata = metadata(&test_path).unwrap();
+        let modified_time = FileTime::from_last_modification_time(&metadata);
+        assert_eq!(modified_time.unix_seconds(), FileTime::now().unix_seconds());
+        fs::remove_file(test_path).unwrap();
+    }
 }
