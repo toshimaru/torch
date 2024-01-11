00338ebd6e7152db07869a83f46fb3206dc92836
toshimaru <me@toshimaru.net>
2024-01-12 08:49:30 +0900

    refactor: Create `mkdir_touch` function
    `mkdir_touch` returns `bool`.

--- src/main.rs
+++ src/main.rs
@@ -14,26 +14,36 @@ fn main() {
     let args = Args::parse();
 
     for path in args.paths {
-        let p = Path::new(&path);
+        mkdir_touch(&path);
+    }
+}
 
-        // Create directory if it contains directories
-        if path.contains('/') {
-            if let Some(dir) = p.parent() {
-                match mkdir(dir) {
-                    Ok(_) => {},
-                    Err(e) => {
-                        println!("Error creating directory({}): {}", dir.display(), e)
-                    }
+fn mkdir_touch(path: &str) -> bool {
+    let p = Path::new(&path);
+
+    // Create directory if it contains directories
+    if path.contains('/') {
+        if let Some(dir) = p.parent() {
+            match mkdir(dir) {
+                Ok(_) => {}
+                Err(e) => {
+                    println!("Error creating directory({}): {}", dir.display(), e);
+                    return false;
                 }
             }
         }
+    }
 
-        // Create file
-        match touch(p) {
-            Ok(_) => {},
-            Err(e) => println!("Error creating file({}): {}", path, e),
+    // Create file
+    match touch(p) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("Error creating file({}): {}", path, e);
+            return false;
         }
     }
+
+    true
 }
 
 fn mkdir(dir: &Path) -> Result<()> {
@@ -71,7 +81,7 @@ mod tests {
 
     #[test]
     fn test_touch_updates_timestamp() {
-        let test_path =  Path::new("test_touch_updates_timestamp");
+        let test_path = Path::new("test_touch_updates_timestamp");
         File::create(test_path).unwrap();
         thread::sleep(Duration::from_secs(1));
         assert!(touch(test_path).is_ok());
@@ -96,5 +106,44 @@ mod tests {
         create_dir_all(existing_dir).unwrap();
         assert!(mkdir(existing_dir).is_ok());
         remove_dir_all(existing_dir).unwrap();
+    }
+
+    #[test]
+    fn test_mkdir_touch_with_directory1() {
+        let dir = "test_mkdir_touch_with_directory1";
+        let path = format!("{}/{}", dir, "a.txt");
+        remove_dir_all(dir).ok();
+        assert!(mkdir_touch(&path));
+        assert!(Path::new(&path).exists());
+        remove_dir_all(dir).unwrap();
+    }
+
+    #[test]
+    fn test_mkdir_touch_with_directory2() {
+        let dir = "test_mkdir_touch_with_directory2";
+        let path = format!("{}/{}", dir, "a/b/c.txt");
+        remove_dir_all(dir).ok();
+        assert!(mkdir_touch(&path));
+        assert!(Path::new(&path).exists());
+        remove_dir_all(dir).unwrap();
+    }
+
+    #[test]
+    fn test_mkdir_touch_without_directory() {
+        let path = "test_mkdir_touch_without_directory";
+        remove_file(path).ok();
+        assert!(mkdir_touch(path));
+        assert!(Path::new(path).exists());
+        remove_file(path).unwrap();
+    }
+
+    #[test]
+    fn test_mkdir_touch_error() {
+        let path = "test_mkdir_touch_error";
+        File::create(path).unwrap();
+        let create_path = format!("{}/{}", path, "a.txt");
+        assert!(!mkdir_touch(&create_path));
+        assert!(!Path::new(&create_path).exists());
+        remove_file(path).unwrap();
     }
 }
